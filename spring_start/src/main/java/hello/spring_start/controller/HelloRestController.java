0cc201c4e05f82d37dfaac367160b33f779c89a1
Jayden <pyc8992dev@gmail.com>
2023-09-18 20:36:18 +0900

    adds service object

--- spring_start/src/main/java/hello/spring_start/controller/HelloRestController.java
+++ spring_start/src/main/java/hello/spring_start/controller/HelloRestController.java
@@ -1,12 +1,17 @@
 package hello.spring_start.controller;
 
+import hello.spring_start.service.SimpleHelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 //@RestController
 public class HelloRestController {
 //    @GetMapping("/v2/hello")
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ spring_start/src/main/java/hello/spring_start/service/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package hello.spring_start.service;
+
+public class SimpleHelloService {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
