46089942429783f164c4fb5b230becd4b1f675f7
Jayden <pyc8992dev@gmail.com>
2023-09-18 20:58:38 +0900

    애너테이션 매핑 정보 사용 for DispatcherServlet

--- spring_start/src/main/java/hello/spring_start/controller/HelloRestController.java
+++ spring_start/src/main/java/hello/spring_start/controller/HelloRestController.java
@@ -1,10 +1,15 @@
 package hello.spring_start.controller;
 
 import hello.spring_start.service.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 //@RestController
+@RequestMapping("/hello")
 public class HelloRestController {
     private final HelloService helloService;
 
@@ -12,7 +17,8 @@ public class HelloRestController {
         this.helloService = helloService;
     }
 
-    //    @GetMapping("/v2/hello")
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
