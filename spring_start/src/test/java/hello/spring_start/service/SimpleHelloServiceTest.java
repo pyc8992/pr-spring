469fd17313a1590fec492d20c0dc4ebcd7aa24d8
Jayden <pyc8992dev@gmail.com>
2023-09-22 23:50:24 +0900

    meta annotation

--- spring_start/src/test/java/hello/spring_start/service/SimpleHelloServiceTest.java
+++ spring_start/src/test/java/hello/spring_start/service/SimpleHelloServiceTest.java
@@ -3,10 +3,27 @@ package hello.spring_start.service;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest {
+}
+
 class SimpleHelloServiceTest {
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
