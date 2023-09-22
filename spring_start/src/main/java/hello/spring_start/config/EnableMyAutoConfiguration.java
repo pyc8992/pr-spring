b0bf28e9133a723a7cfab18d95cb55455b15ab8a
Jayden <pyc8992dev@gmail.com>
2023-09-23 00:14:21 +0900

    인프라 빈 구성 정보의 분리

---
+++ spring_start/src/main/java/hello/spring_start/config/EnableMyAutoConfiguration.java
@@ -0,0 +1,16 @@
+package hello.spring_start.config;
+
+import hello.spring_start.config.autoconfig.DispatcherServletConfig;
+import hello.spring_start.config.autoconfig.TomcatWebServerConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+public @interface EnableMyAutoConfiguration {
+}
---
+++ spring_start/src/main/java/hello/spring_start/config/autoconfig/DispatcherServletConfig.java
@@ -0,0 +1,15 @@
+package hello.spring_start.config.autoconfig;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class DispatcherServletConfig {
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
