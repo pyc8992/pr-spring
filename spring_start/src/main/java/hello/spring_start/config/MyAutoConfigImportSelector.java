63baf498eba6c0ef3d8a98aa09fcb14a47a089b4
Jayden <pyc8992dev@gmail.com>
2023-09-25 16:49:14 +0900

    자동 구성 적용

---
+++ spring_start/src/main/java/hello/spring_start/config/MyAutoConfigImportSelector.java
@@ -0,0 +1,30 @@
+package hello.spring_start.config;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        List<String> autoConfigs = new ArrayList<>();
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+//        return new String[] {
+//                "hello.spring_start.config.autoconfig.DispatcherServletConfig",
+//                "hello.spring_start.config.autoconfig.TomcatWebServerConfig"
+//        };
+
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+        return autoConfigs.toArray(new String[0]);
+    }
+}
