145165d12f50f96ccef5b99ad56a398475ea96b9
Jayden <pyc8992dev@gmail.com>
2023-09-26 18:26:42 +0900

    adds @Value & PropertySourcesPlaceholderConfigurer

---
+++ spring_start/src/main/java/hello/spring_start/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package hello.spring_start.config.autoconfig;
+
+import hello.spring_start.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
--- spring_start/src/main/java/hello/spring_start/config/autoconfig/TomcatWebServerConfig.java
+++ spring_start/src/main/java/hello/spring_start/config/autoconfig/TomcatWebServerConfig.java
@@ -2,21 +2,25 @@ package hello.spring_start.config.autoconfig;
 
 import hello.spring_start.config.ConditionalMyOnClass;
 import hello.spring_start.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+    @Value("${contextPath}")
+    private String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
