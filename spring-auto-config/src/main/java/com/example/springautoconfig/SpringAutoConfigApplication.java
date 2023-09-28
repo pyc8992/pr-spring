3daeb26915e5924c421f8dabda5dd34e0fe2d15b
Jayden <pyc8992dev@gmail.com>
2023-09-28 18:34:11 +0900

    core 자동 구성 확인

--- spring-auto-config/src/main/java/com/example/springautoconfig/SpringAutoConfigApplication.java
+++ spring-auto-config/src/main/java/com/example/springautoconfig/SpringAutoConfigApplication.java
@@ -8,6 +8,25 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAutoConfigApplication {
+
+    @Bean
+    ApplicationRunner run(ConditionEvaluationReport report) {
+        return args -> {
+            System.out.println(
+                    report.getConditionAndOutcomesBySource().entrySet().stream()
+                            .filter(co -> co.getValue().isFullMatch())
+                            .filter(co -> !co.getKey().contains("Jmx"))
+                            .peek(co -> {
+                                System.out.println(co.getKey());
+                                co.getValue().forEach(c -> {
+                                    System.out.println("\t" + c.getOutcome());
+                                });
+
+                            }).count()
+            );
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringAutoConfigApplication.class, args);
     }
