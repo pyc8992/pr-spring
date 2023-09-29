8bdfc352fc8a5dcaf010329520deea9721d94f65
Jayden <pyc8992dev@gmail.com>
2023-09-29 09:06:48 +0900

    spring jdbc 자동 구성 확인

--- spring-auto-config/src/main/java/com/example/springautoconfig/SpringAutoConfigApplication.java
+++ spring-auto-config/src/main/java/com/example/springautoconfig/SpringAutoConfigApplication.java
@@ -11,20 +11,19 @@ public class SpringAutoConfigApplication {
 
     @Bean
     ApplicationRunner run(ConditionEvaluationReport report) {
-        return args -> {
-            System.out.println(
-                    report.getConditionAndOutcomesBySource().entrySet().stream()
-                            .filter(co -> co.getValue().isFullMatch())
-                            .filter(co -> !co.getKey().contains("Jmx"))
-                            .peek(co -> {
-                                System.out.println(co.getKey());
-                                co.getValue().forEach(c -> {
-                                    System.out.println("\t" + c.getOutcome());
-                                });
-
-                            }).count()
-            );
-        };
+        return args -> System.out.println(
+                report.getConditionAndOutcomesBySource().entrySet()
+                        .stream()
+                        .filter(co -> co.getValue().isFullMatch())
+                        .filter(co -> !co.getKey().contains("Jmx"))
+                        .peek(co -> {
+                            System.out.println(co.getKey());
+                            co.getValue().forEach(c -> {
+                                System.out.println("\t" + c.getOutcome());
+                            });
+                            System.out.println("\n");
+                        }).count()
+        );
     }
 
     public static void main(String[] args) {
