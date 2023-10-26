2f501220684bd16ffa5c134481d8452052b95cac
Jayden <pyc8992dev@gmail.com>
2023-10-26 11:08:15 +0900

    동시성 이슈 처리 : synchronized keyword

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -13,8 +13,25 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+//    @Transactional
+//    public void decrease(Long id, Long quantity) {
+//        // Stock 조회
+//        // 재고 감소
+//        // 갱신된 값 저장
+//        Stock stock = stockRepository.findById(id).orElseThrow();
+//        stock.decrease(quantity);
+//        stockRepository.saveAndFlush(stock);
+//    }
+
+    /**
+     * 동시성 이슈 처리
+     * synchronized keyword 활용
+     * - 자바의 기본 키워드인 synchronized를 활용하면 하나의 프로세스에 하나의 스레드만 동작하도록 보장
+     * 문제점
+     * - 보통의 application 서버들은 2대 이상을 구축하는데 다른 서버에서는 해당 키워드를 알 수 없기 때문에 race condition 이슈는 여전히 존재
+     */
     @Transactional
-    public void decrease(Long id, Long quantity) {
+    public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고 감소
         // 갱신된 값 저장
