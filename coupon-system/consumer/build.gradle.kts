9ea91b8ce9ba66c48cea1084087ac5c65cccad06
Jayden <pyc8992dev@gmail.com>
2023-10-27 15:46:23 +0900

    adds consumer module [kafka를 통한 쿠폰 처리]

---
+++ coupon-system/consumer/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  java
+  id("org.springframework.boot") version "3.0.12"
+  id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.kafka:spring-kafka")
+  runtimeOnly("com.mysql:mysql-connector-j")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
